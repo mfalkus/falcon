91fb26f262cb9b510e72897e7f6985faf6649d58
Martin Falkus <martin@goldbyte.co.uk>
2016-01-11 19:38:14 +0000

    Add very basic favicon
    It's PNG so older IE's aren't supported, but we haven't been making
    much effort to support these anyway.

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, minimum-scale=1.0">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory') ?>/favicon.png"  type="image/png" />
 
 <?php wp_head(); ?>
 </head>

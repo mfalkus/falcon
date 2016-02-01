3fc91c048093afe349e79322a5b6ee6d72bc0301
Martin Falkus <martin@goldbyte.co.uk>
2016-02-01 15:54:06 +0000

    Add template for 'standard' post display
    Also give just a preview of standard posts unless is_single

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ get_header(); ?>
         <main class="main-site" role="main">
 
         <?php while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'content-templates/content', get_post_format() ); ?>
+            <?php get_template_part(
+                'content-templates/content',
+                get_post_format() ? : 'standard'
+            ); ?>
         <?php endwhile; ?>
 
         </main>

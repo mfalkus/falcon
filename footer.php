1d2a52ab64d898f2f6d339388771191f25f09987
Martin Falkus <martin@goldbyte.co.uk>
2016-02-27 19:02:17 +0000

    Add a copyright to the footer

--- footer.php
+++ footer.php
@@ -7,7 +7,11 @@
 
 	<footer class="footer-wrapper" role="contentinfo">
     <p class="micro">
-        <?php bloginfo('name') ?> &dash; <?php bloginfo('description'); ?><br />
+        <?php bloginfo('name') ?>
+        &dash; <?php bloginfo('description'); ?>
+        &dash; Copyright &copy; <?php echo date("Y"); ?>
+
+        <br />
         <a href="https://github.com/mfalkus/falcon">Get this theme on GitHub</a>
     </p>
 	</footer>
